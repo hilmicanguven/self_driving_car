4714d1e4827761dc0851fc265f8d8b37cdb3489e
hilmicanguven <47054209+hilmicanguven@users.noreply.github.com>
2020-08-26 16:45:42 +0300

    Train model for self driving car
    In the folder, there is a model for pre-trained for you. It is used to driving car. But of course there is a python file called model.py to train your own model by using your own dataset. Then, using drive.py, you can test your model.
    First, you should install "Udacity Simulator".

---
+++ model.py
@@ -0,0 +1,111 @@
+import csv
+import cv2
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers import Conv2D
+from keras.layers.pooling import MaxPooling2D
+import numpy as np
+
+lines = []
+
+with open('./data/driving_log.csv') as csvfile:
+  reader = csv.reader(csvfile)
+  for line in reader:
+    lines.append(line)
+
+#with open('./data_track2/driving_log.csv') as csvfile:
+#  reader = csv.reader(csvfile)
+#  for line in reader:
+#    lines.append(line)
+
+images = []
+measurements = []
+
+print("Getting data....")
+correction = 0.2
+for line in lines:
+  for i in range(3):
+    source_path = line[i]
+    print(line[i])
+    filename = source_path.split('/')[-1]
+    print(filename)
+    current_path = filename
+    print(current_path)
+    image = cv2.imread(current_path)
+    image_rgb = (cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    images.append(image_rgb)
+
+    measurement = float(line[3])
+
+    if (i == 1):
+      measurement = measurement + correction
+    
+    if (i == 2):
+      measurement = measurement - correction
+
+    measurements.append(measurement)
+
+
+augmentated_images, augmentated_measurements = [], []
+
+for image, measurement in zip(images, measurements):
+  augmentated_images.append(image)
+  augmentated_measurements.append(measurement)
+  augmentated_images.append(cv2.flip(image, 1))
+  augmentated_measurements.append(measurement * -1)
+
+X_train = np.array(augmentated_images)
+y_train = np.array(augmentated_measurements)
+
+
+
+
+# Nvidia's CNN architecture
+model = Sequential()
+model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=X_train[0].shape))
+
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Dropout(0.2))
+model.add(Conv2D(24, (5, 5), activation="relu", padding='same'))
+model.add(MaxPooling2D())
+model.add(Conv2D(36, (5, 5), activation="relu", padding='same'))
+model.add(MaxPooling2D())
+model.add(Conv2D(48, (5, 5), activation="relu", padding='same'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+model.add(Conv2D(64, (3, 3), activation="relu", padding='same'))
+model.add(MaxPooling2D())
+model.add(Conv2D(64, (3, 3), activation="relu", padding='same'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(1164))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+#model = Sequential()
+#model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+
+#model.add(Cropping2D(cropping=((80, 25), (0, 0))))
+
+#model.add(Convolution2D(6, 5, 5, activation="relu"))
+#model.add(MaxPooling2D(j))
+
+#model.add(Convolution2D(6, 5, 5, activation="relu"))
+#model.add(MaxPooling2D())
+
+# use dropout to eliminate overfitting and improve the validation loss
+#model.add(Dropout(.3))
+
+#model.add(Flatten())
+#model.add(Dense(120))
+#model.add(Dense(84))
+#model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+print(model.summary())
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
+
+model.save('h1.h5')
+
